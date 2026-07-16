b3f17dc36ad2943edebc6e99bf14ccc433dfc6e7
deevseek <nugraha.deev@gmail.com>
2026-07-16 22:08:12 +0700

    fix(ui): render functional button component

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,1 +1,23 @@
-{{ $slot }}
+@props([
+    'type' => 'submit',
+    'variant' => 'primary',
+    'href' => null,
+])
+
+@php
+    $variantClass = match ($variant) {
+        'secondary' => 'btn-secondary',
+        'danger' => 'btn-danger',
+        default => 'btn-primary',
+    };
+@endphp
+
+@if ($href)
+    <a href="{{ $href }}" {{ $attributes->class(['btn', $variantClass]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button type="{{ $type }}" {{ $attributes->class(['btn', $variantClass]) }}>
+        {{ $slot }}
+    </button>
+@endif
