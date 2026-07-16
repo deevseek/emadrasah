2b94032565736199c19517fad220acc39ba96fc6
deevseek <nugraha.deev@gmail.com>
2026-07-16 22:08:55 +0700

    fix(ui): render functional badge component

--- resources/views/components/ui/badge.blade.php
+++ resources/views/components/ui/badge.blade.php
@@ -1,1 +1,17 @@
-{{ $slot }}
+@props([
+    'variant' => 'muted',
+])
+
+@php
+    $classes = match ($variant) {
+        'success' => 'badge-success',
+        'warning' => 'badge-warning',
+        'danger' => 'bg-rose-50 text-rose-700 ring-1 ring-rose-200',
+        'info' => 'bg-sky-50 text-sky-700 ring-1 ring-sky-200',
+        default => 'badge-muted',
+    };
+@endphp
+
+<span {{ $attributes->class(['badge', $classes]) }}>
+    {{ $slot }}
+</span>
