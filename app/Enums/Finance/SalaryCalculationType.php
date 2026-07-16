bbea0a231caed138146b82500c385e9ddc5176ec
deevseek <nugraha.deev@gmail.com>
2026-07-16 21:51:30 +0700

    fix(finance): add salary calculation enum file

---
+++ app/Enums/Finance/SalaryCalculationType.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums\Finance;
+
+enum SalaryCalculationType: string
+{
+    case Fixed = 'fixed';
+    case Percentage = 'percentage';
+    case Attendance = 'attendance';
+    case Manual = 'manual';
+}
