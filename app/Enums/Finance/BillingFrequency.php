de9462e3366fa31b0d2a4d6d1a8cdae2fb55d988
deevseek <nugraha.deev@gmail.com>
2026-07-16 21:49:48 +0700

    fix(finance): add billing frequency enum file

---
+++ app/Enums/Finance/BillingFrequency.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums\Finance;
+
+enum BillingFrequency: string
+{
+    case Once = 'sekali';
+    case Monthly = 'bulanan';
+    case Semester = 'semester';
+    case Yearly = 'tahunan';
+    case Incidental = 'insidental';
+}
