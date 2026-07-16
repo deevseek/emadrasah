40ec9352d3ffc1705d9ae21478005d06a1e94968
deevseek <nugraha.deev@gmail.com>
2026-07-16 22:09:27 +0700

    fix(ui): render functional table component

--- resources/views/components/ui/table.blade.php
+++ resources/views/components/ui/table.blade.php
@@ -1,1 +1,9 @@
-{{ $slot }}
+@props([
+    'striped' => false,
+])
+
+<div class="table-wrap">
+    <table {{ $attributes->class(['data-table', '[&_tbody_tr:nth-child(even)]:bg-slate-50/50' => $striped]) }}>
+        {{ $slot }}
+    </table>
+</div>
